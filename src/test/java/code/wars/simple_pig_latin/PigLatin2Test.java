882a9e567968676de17c57b41dbc371bb32e31f5
RicardooCorreia <ricardocorreiap@gmail.com>
2022-02-09 09:26:22 +0000

    Added Pig Latin and Credit Card Mask

---
+++ src/test/java/code/wars/simple_pig_latin/PigLatin2Test.java
@@ -0,0 +1,52 @@
+package code.wars.simple_pig_latin;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class PigLatin2Test {
+
+    public static Stream<Arguments> phrases() {
+
+        return Stream.of(
+                arguments("Pig latin is cool", "igPay atinlay siay oolcay"),
+                arguments("This is my string", "hisTay siay ymay tringsay")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("phrases")
+    void pigIt_moveFirstLetterAndAddSuffix(
+            // Given
+            String input,
+            String expected) {
+
+        // When
+        final String result = PigLatin2.pigIt(input);
+
+        // Then
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void pigIt_whenSpecialCharacters_doesNotLatinIt() {
+
+        // Given
+        final String input = "![] . /&%";
+
+
+        // When
+        final String result = PigLatin2.pigIt(input);
+
+        // Then
+        assertThat(result)
+                .isEqualTo(input);
+    }
+}
