f184fc49980cc566b92d4265fd51cb5986034e0a
RicardooCorreia <ricardocorreiap@gmail.com>
2024-04-19 09:11:19 +0100

    Added daily katas

---
+++ src/main/java/code/wars/a_plus_b_equals_123/Dinglemouse.java
@@ -0,0 +1,17 @@
+package code.wars.a_plus_b_equals_123;
+
+public class Dinglemouse {
+
+    private static final int TARGET = 123;
+
+    public static long int123(final int a) {
+
+        final int difference = 123 - a;
+
+        if (difference >= 0) {
+            return difference;
+        } else {
+            return ((long) Integer.MAX_VALUE) * 2 + 2 + difference;
+        }
+    }
+}
---
+++ src/test/java/code/wars/a_plus_b_equals_123/DinglemouseTest.java
@@ -0,0 +1,72 @@
+package code.wars.a_plus_b_equals_123;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DinglemouseTest {
+
+    @Test
+    public void int123_whenAisLesserThan123_thenReturnCorrectB() {
+
+        // Given
+        final int input = 0;
+
+        // When
+        final long result = Dinglemouse.int123(input);
+g
+        // Then
+        assertThat(result)
+                .isGreaterThanOrEqualTo(0);
+        assertThat((int) (input + result))
+                .isEqualTo(123);
+    }
+
+    @Test
+    public void int123_whenAisAlready123_thenReturnZero() {
+
+        // Given
+        final int input = 123;
+
+        // When
+        final long result = Dinglemouse.int123(input);
+
+        // Then
+        assertThat(result)
+                .isGreaterThanOrEqualTo(0);
+        assertThat((int) (input + result))
+                .isEqualTo(123);
+    }
+
+    @Test
+    public void int123_whenAisBiggerThan123_thenReturnPositiveBThatAddsTo123() {
+
+        // Given
+        final int input = 500;
+
+        // When
+        final long result = Dinglemouse.int123(input);
+
+        // Then
+        assertThat(result)
+                .isGreaterThanOrEqualTo(0);
+        assertThat((int) (input + result))
+                .isEqualTo(123);
+    }
+
+    @Test
+    public void int123_whenAisMuchBiggerThan123_thenReturnPositiveBThatAddsTo123() {
+
+        // Given
+        final int input = 99999;
+
+        // When
+        final long result = Dinglemouse.int123(input);
+
+        // Then
+        assertThat(result)
+                .isGreaterThanOrEqualTo(0);
+        assertThat((int) (input + result))
+                .isEqualTo(123);
+    }
+}
