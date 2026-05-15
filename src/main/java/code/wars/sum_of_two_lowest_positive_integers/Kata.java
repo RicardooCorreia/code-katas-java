1f843b1d4ed995207da369ae753d90788c32d04e
Ricardo Correia <ricardocorreiap@gmail.com>
2026-05-15 09:42:10 +0100

    testing

--- src/main/java/code/wars/sum_of_two_lowest_positive_integers/Kata.java
+++ src/main/java/code/wars/sum_of_two_lowest_positive_integers/Kata.java
@@ -11,8 +11,8 @@ public class Kata {
             return 0L;
         }
 
-        long minNumber1 = min(numbers[0], numbers[1]);
-        long minNumber2 = max(numbers[0], numbers[1]);
+        var minNumber1 = min(numbers[0], numbers[1]);
+        var minNumber2 = max(numbers[0], numbers[1]);
 
         for (int i = 2; i < numbers.length; i++) {
             final long number = numbers[i];
